fd5899e71c91acbd7bc551854ffb41cd42baca0f
NATARAJAN V <vnadarajan15@gmail.com>
2026-08-30 17:55:52 +0530

    Time: 2 ms (100.00%) | Memory: 86.7 MB (60.18%) - LeetSync

--- 2212-removing-minimum-and-maximum-from-array/removing-minimum-and-maximum-from-array.java
+++ 2212-removing-minimum-and-maximum-from-array/removing-minimum-and-maximum-from-array.java
@@ -5,24 +5,26 @@ class Solution {
         if(n <= 2){
             return n;
         }
-        ArrayList<Integer> list = new ArrayList<>();
-        for(int i=0; i<nums.length; i++){
-            list.add(nums[i]);
-        }
-        int minimum = Collections.min(list);
-        int maximum = Collections.max(list);
-        int minIndex = list.indexOf(minimum);
-        int maxIndex = list.indexOf(maximum);
+        int minimum = Integer.MAX_VALUE;
+        int maximum = Integer.MIN_VALUE;
+        int minIndex = 0;
+        int maxIndex = 0;
 
-        //if(minIndex <= (n/2) && maxIndex <= (n/2)){
-            int deletions1 = Math.max(minIndex + 1,maxIndex + 1);
-        //}
-        //else if(minIndex > (n/2) && maxIndex > (n/2)){
-            int deletions2 = Math.max(n - minIndex,n - maxIndex);
-        //}
-        //else{
-            int deletions3 = Math.min(minIndex + 1,n - minIndex) + Math.min(maxIndex + 1,n - maxIndex);
-        //}
+        for(int i=0; i<nums.length; i++){
+            if(nums[i] < minimum){
+                minimum = nums[i];
+                minIndex = i;
+            }
+            if(nums[i] > maximum){
+                maximum = nums[i];
+                maxIndex = i;
+            }
+        }
+
+        int deletions1 = Math.max(minIndex + 1,maxIndex + 1);
+        int deletions2 = Math.max(n - minIndex,n - maxIndex);
+        int deletions3 = Math.min(minIndex + 1,n - minIndex) + Math.min(maxIndex + 1,n - maxIndex);
+        
         return Math.min(deletions3,Math.min(deletions1,deletions2));
     }
 }
