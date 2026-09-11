23f59f2c29f0ab9f9b2d260b98e581105f13c0de
NATARAJAN V <vnadarajan15@gmail.com>
2026-09-11 20:37:30 +0530

    Time: 3 ms (76.61%) | Memory: 46.6 MB (36.43%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.java
+++ 13-roman-to-integer/roman-to-integer.java
@@ -1,18 +1,35 @@
 class Solution {
     public int romanToInt(String s) {
-        HashMap<Character,Integer> map = new HashMap<>();
-        map.put('I',1);
-        map.put('V',5);
-        map.put('X',10);
-        map.put('L',50);
-        map.put('C',100);
-        map.put('D',500);
-        map.put('M',1000);
-        int value = map.get(s.charAt(s.length()-1));
+        // HashMap<Character,Integer> map = new HashMap<>();
+        // map.put('I',1);
+        // map.put('V',5);
+        // map.put('X',10);
+        // map.put('L',50);
+        // map.put('C',100);
+        // map.put('D',500);
+        // map.put('M',1000);
+        // int value = map.get(s.charAt(s.length()-1));
+        // int previousValue = value;
+        // for(int i = s.length()-2; i>=0; i--)
+        // {
+        //     int charValue = map.get(s.charAt(i));
+        //     value += (charValue >= previousValue) ? charValue : -charValue;
+        //     previousValue = charValue;
+        // }
+        // return value;
+        int[] arr = new int[26];
+        arr['i'-'a'] = 1;
+        arr['v'-'a'] = 5;
+        arr['x'-'a'] = 10;
+        arr['l'-'a'] = 50;
+        arr['c'-'a'] = 100;
+        arr['d'-'a'] = 500;
+        arr['m'-'a'] = 1000;
+        int value = arr[Character.toLowerCase(s.charAt(s.length()-1)) - 'a'];
         int previousValue = value;
         for(int i = s.length()-2; i>=0; i--)
         {
-            int charValue = map.get(s.charAt(i));
+            int charValue = arr[Character.toLowerCase(s.charAt(i)) - 'a'];
             value += (charValue >= previousValue) ? charValue : -charValue;
             previousValue = charValue;
         }
