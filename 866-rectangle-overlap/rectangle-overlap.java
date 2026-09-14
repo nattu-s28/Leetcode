3a29cdfb8cec0b502ed564a4a3a4a39796178643
NATARAJAN V <vnadarajan15@gmail.com>
2026-09-14 18:11:42 +0530

    Time: 0 ms (100.00%) | Memory: 42.6 MB (71.82%) - LeetSync

--- 866-rectangle-overlap/rectangle-overlap.java
+++ 866-rectangle-overlap/rectangle-overlap.java
@@ -4,8 +4,9 @@ class Solution {
         int x2 = Math.min(rec1[2],rec2[2]);
         int y1 = Math.max(rec1[1],rec2[1]);
         int y2 = Math.min(rec1[3],rec2[3]);
-        int width = x2 - x1;
-        int height = y2 - y1;
-        return (height > 0 && width > 0);
+        long width = Math.max(0,x2 - x1);
+        long height = Math.max(0,y2 - y1);
+        long area = width * height;
+        return (area > 0) ? true : false;
     }
 }
