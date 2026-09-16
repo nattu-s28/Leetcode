3ff1449b1ece346bb285d3070b542f7fde44a2cd
NATARAJAN V <vnadarajan15@gmail.com>
2026-09-16 14:35:59 +0530

    Time: 4 ms (87.98%) | Memory: 47.4 MB (52.43%) - LeetSync

--- 3206-find-common-elements-between-two-arrays/find-common-elements-between-two-arrays.java
+++ 3206-find-common-elements-between-two-arrays/find-common-elements-between-two-arrays.java
@@ -1,25 +1,28 @@
 class Solution {
     public int[] findIntersectionValues(int[] nums1, int[] nums2) {
-        List<Integer> numsList1 = new ArrayList<>();
-        List<Integer> numsList2 = new ArrayList<>();
-        for(int i=0; i < nums1.length;i++){
-                numsList1.add(nums1[i]);
-        }
-        for(int j=0; j < nums2.length;j++){
-                numsList2.add(nums2[j]);
-        }
-        int cnt1 = 0;
-        for(int num : numsList1){
-            if(numsList2.contains(num)){
-                cnt1++;
+        int arr[] = new int[2];
+        int p1 = 0;
+        int p2 = 0;
+        int count1=0;
+        int count2=0;
+        for(int i=0; i<nums1.length; i++){
+            for(int j=0; j<nums2.length; j++){
+                if(nums1[i]==nums2[j]){
+                    count1++;
+                    break;
+                }
             }
         }
-        int cnt2 = 0;
-        for(int num : numsList2){
-            if(numsList1.contains(num)){
-                cnt2++;
+       for(int i=0; i<nums2.length; i++){
+            for(int j=0; j<nums1.length; j++){
+                if(nums2[i]==nums1[j]){
+                    count2++;
+                    break;
+                }
             }
         }
-        return new int[]{cnt1,cnt2};
+        arr[0] = count1;
+        arr[1] = count2;
+        return arr;
     }
 }
