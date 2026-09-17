b372c084842c70ee783ab717f7bc279212d23b67
NATARAJAN V <vnadarajan15@gmail.com>
2026-09-17 11:49:43 +0530

    Time: 3 ms (73.65%) | Memory: 52.8 MB (29.96%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.java
+++ 229-majority-element-ii/majority-element-ii.java
@@ -1,17 +1,41 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        HashMap<Integer,Integer> map = new HashMap<>();
-        for(int i=0; i<nums.length; i++){
-            map.put(nums[i],map.getOrDefault(nums[i],0)+1);
-        }
         List<Integer> list = new ArrayList<>();
-        for(int num : map.keySet()){
-            if(map.get(num) > nums.length/3){
-                list.add(num);
-                if(list.size() == 2){
-                    break;
-                }
+        if(nums.length < 2){
+            list.add(nums[0]);
+            return list;
+        }
+        int element1 = Integer.MIN_VALUE;
+        int element2 = Integer.MIN_VALUE;
+        int cnt1 = 0;
+        int cnt2 = 0;
+        for(int i=0; i<nums.length; i++){
+            if(cnt1 == 0 && nums[i] != element2){
+                cnt1 = 1;
+                element1 = nums[i];
             }
+            else if(cnt2 == 0 && nums[i] != element1){
+                cnt2 = 1;
+                element2 = nums[i];
+            }
+            else if(element1 == nums[i]) cnt1++;
+            else if(element2 == nums[i]) cnt2++;
+            else{
+                cnt1--;
+                cnt2--;
+            }
+        }
+        cnt1 = 0;
+        cnt2 = 0;
+        for(int i=0; i<nums.length; i++){
+            if(element1 == nums[i]) cnt1++;
+            if(element2 == nums[i]) cnt2++;
+        }
+        if(cnt1 > nums.length/3){
+            list.add(element1);
+        }
+        if(cnt2 > nums.length/3){
+            list.add(element2);
         }
         return list;
     }
